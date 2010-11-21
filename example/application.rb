5d1048e5d43b37c72da6607aa5af39bd583f6c9f
James Coglan <jcoglan@gmail.com>
2010-11-21 13:56:38 +0000

    Move Primer setup for the example app into its own file.

--- example/application.rb
+++ example/application.rb
@@ -5,28 +5,10 @@ require 'active_record'
 class Application < Sinatra::Base
   ROOT = File.expand_path(File.dirname(__FILE__))
   
-  require ROOT + '/../lib/primer'
-  Primer.cache = Primer::Cache::Redis.new
-  
-  Primer.real_time = true
-  Primer::RealTime.bayeux_server = 'http://0.0.0.0:9292'
-  Primer::RealTime.password = 'omg_rofl_scale'
-  
+  require ROOT + '/config/primer'
   require ROOT + '/models/connection'
   require ROOT + '/models/blog_post'
   
-  # Set up cache generators
-  Primer.cache.routes do
-    get '/posts/:id/date' do
-      BlogPost.find(params[:id]).created_at.strftime('%A %e %B %Y')
-    end
-    
-    get '/posts/:id/title' do
-      BlogPost.find(params[:id]).title.upcase
-    end
-  end
-  
-  # Configure Sinatra
   set :reload_templates, true
   set :static, true
   set :public, ROOT + '/public'
