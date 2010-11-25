49f7a423f579fb99dc23151191e1aec3dc297e19
James Coglan <jcoglan@gmail.com>
2010-11-25 01:06:15 +0000

    Make Worker pick up ActiveRecord object notifications.

--- lib/primer/worker.rb
+++ lib/primer/worker.rb
@@ -3,7 +3,7 @@ module Primer
     
     class ConfigError < StandardError ; end
     
-    autoload :ActiveRecordAgent, 'primer/worker/active_record_agent'
+    autoload :ActiveRecordAgent, ROOT + '/primer/worker/active_record_agent'
     
     def run
       raise ConfigError.new('No cache present') unless Primer.cache
@@ -14,11 +14,16 @@ module Primer
       puts
       
       EM.run {
-        Primer.bus.subscribe :changes do |message|
-          puts "[changes] #{ message.inspect }"
+        Primer.bus.subscribe :active_record do |args|
+          puts "[active_record] #{ args.inspect }"
+        end
+        Primer.bus.subscribe :changes do |args|
+          puts "[changes] #{ args.inspect }"
         end
         
+        ActiveRecordAgent.bind_to_bus
         Primer.cache.bind_to_bus
+        
         puts "Listening for messages..."
         puts
       }
